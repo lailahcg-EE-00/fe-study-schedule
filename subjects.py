b8b7a03a128d27b2c5766e2ff4720912ebc923f4
lailahcg-EE-00 <lailahcg@gmail.com>
2026-09-08 14:51:25 -0700

    Create subjects.py

---
+++ subjects.py
@@ -0,0 +1,288 @@
+SUBJECTS = [
+
+  {
+    "topic": "Mathematics",
+    "days": 1,
+    "tasks": [
+      "Complex Numbers 10 QQ",
+      "Analytic Geometry 10 QQ",
+      "Calculus 10 QQ",
+      "Differential Equations 10 QQ",
+      "Linear Algebra 10 QQ"
+      ]
+    },
+  {
+    "topic": "Probability & Statistics",
+    "days": 4,
+    "tasks": [
+      "Measures of Central Tendencies and Dispersions videos",
+      "Probability Distributions videos",
+      "Expected Values videos",
+      "Measures of Central Tendencies and Dispersions 10 QQ",
+      "Probability Distributions and Expected Values 10 QQ"
+      ]
+    },
+  {
+    "topic": "Engineering Ethics",
+      "days": 1,
+      "tasks": [
+      "Ethics 10 QQ"
+      ]
+    },
+  {
+    "topic": "Engineering Economics",
+    "days": 7,
+    "tasks": [
+      "Interest Examples",
+      "Interest Tables",
+      "Cash Flow Diagram Basics",
+      "Present and Future Worth",
+      "Annual Equivalent Uniform Payments",
+      "Cash Flow Example",
+      "Future Worth and Nominal Interest Example",
+      "Gradient and Present Worth",
+      "Gradient and Future Worth",
+      "Straight Line Depreciation",
+      "Modified Accelerated Cost Recovery System (MACRS)",
+      "Capitalized Cost",
+      "Engineering Economics 10 QQ"
+      ]
+    },
+  {
+    "topic": "Properties of Electrical Materials",
+    "days": 16,
+    "tasks": [
+      "Semiconductor Materials - Tunneling",
+      "Semiconductor Materials - Diffusion and Drift Current",
+      "Semiconductor Materials - Diffusion Coefficient",
+      "Semiconductor Materials - Energy Bands",
+      "Semiconductor Materials - Doping Bands",
+      "Semiconductor Materials - P-N Theory",
+      "Semiconductor Materials - P-N Contact Potential",
+      "Electrical - Resistivity",
+      "Electrical - Conductivity",
+      "Electrical - Resistivity Temperature Dependence",
+      "Electrical - Resistance Temperature Dependence",
+      "Electrical - Permittivity",
+      "Electrical - Magnetic Permeability",
+      "Electrical - Noise",
+      "Thermal - Conductivity",
+      "Thermal - Expansion",
+      "Properties of Electrical Materials 10 QQ"
+      ]
+    },
+  {
+    "topic": "Circuit Analysis",
+    "days": 10,
+    "tasks": [
+      "Kirchhoff's Current (KCL) and Voltage (KVL) Law",
+      "Series and Parallel Equivalent Circuits",
+      "Thevenin and Norton Theorems - Part 1",
+      "Thevenin and Norton Theorems - Part 2",
+      "Loop Analysis",
+      "Nodal Analysis",
+      "Super Mesh Analysis",
+      "Super Node Analysis",
+      "Sine Wave Analysis",
+      "Square Wave Analysis",
+      "Sawtooth Wave Analysis",
+      "Phasors",
+      "Phasors and Superposition",
+      "Impedance - Part 1",
+      "Impedance - Part 2",
+      "Circuit Analysis 10 QQ"
+      ]
+    },
+  {
+    "topic": "Linear Systems",
+    "days": 4,
+    "tasks": [
+      "Linear Systems Introduction",
+      "Transient Response - RC Circuit Example",
+      "Laplace Transforms",
+      "Resonance",
+      "RL Circuits",
+      "Transfer Function",
+      "Resonance, Quality Factor, and Bandwidth",
+      "Linear Systems 10 QQ"
+      ]
+    },
+  {
+    "topic": "Signal Processing",
+    "days": 6,
+    "tasks": [
+      "Signal Processing Introduction",
+      "Energy Signals",
+      "Power Signals",
+      "Continuous Time",
+      "Continuous Time: Laplace Transforms",
+      "Continuous Time: Convolution",
+      "Continuous Time: Total System Response",
+      "Discrete Time and Difference Equations",
+      "Discrete Time: Z Transforms",
+      "Discrete Time: Convolution",
+      "Discrete Time: Total System Response",
+      "Filter Introduction and Analog Filters",
+      "Digital Filters and Sampling",
+      "Signal Processing 10 QQ"
+      ]
+    },
+  {
+    "topic": "Electronics",
+    "days": 13,
+    "tasks": [
+      "Electronics Introduction",
+      "Solid-State Electronics Introduction and Diodes",
+      "Real Diodes",
+      "MOSFET and JFET Transistors",
+      "BJT Transistors and Thyristors",
+      "Amplifiers Introduction",
+      "Differential Amplifiers",
+      "Operational Amplifiers Introduction",
+      "Non-ideal Operational Amplifiers",
+      "Instrumentation Introduction",
+      "Transducers",
+      "Power Electronics Introduction and Rectifiers",
+      "Inverters and Converters",
+      "Electronics 10 QQ"
+      ]
+    },
+  {
+    "topic": "Power Systems",
+    "days": 10,
+    "tasks": [
+      "Complex Power",
+      "Balanced Three-Phase (3-Φ) Systems",
+      "Transformers",
+      "Rotating Machines",
+      "DC Machines",
+      "Voltage Regulation and Voltage Drop",
+      "Lossless Transmission Lines",
+      "Use power systems exams from CSUB to do practice questions"
+      ]
+    },
+  {
+    "topic": "Electromagnetics",
+    "days": 7,
+    "tasks": [
+      "Electromagnetics Introduction",
+      "Electrostatics and Magnetostatics - Vector Analysis",
+      "Electrostatics and Magnetostatics - Vector Calculus Basics",
+      "Electrostatics and Magnetostatics - Coordinate Systems",
+      "Electrostatics and Magnetostatics - Electrostatics",
+      "Electrostatics and Magnetostatics - Gauss's Law",
+      "Electrostatics and Magnetostatics - Magnetostatics",
+      "Maxwell Equations for Time-Varying Fields",
+      "Electrodynamics - Numerical Example",
+      "Electrodynamics - Waves",
+      "Electrodynamics - Wave Propagation",
+      "Transmission Lines - High Frequency",
+      "Electromagnetics 10 QQ"
+      ]
+    },
+  {
+    "topic": "Control Systems",
+    "days": 4,
+    "tasks": [
+      "Block Diagrams and Intro to Control Systems",
+      "Bode Plots - Magnitude",
+      "Bode Plots - Phase",
+      "Closed-Loop Response",
+      "Stability",
+      "Root Locus",
+      "Steady State Error",
+      "Controller Performance",
+      "Control Systems 10 QQ"
+    ]
+  },
+ {
+  "topic": "Communications",
+  "days": 8,
+  "tasks": [
+    "Theory and Concepts",
+    "Fourier Transform",
+    "Frequency and Impulse Response",
+    "Parseval's Theorem",
+    "Modulation Theories",
+    "Sampling",
+    "Computer Networks",
+    "Use communications exams from CSUB to do practice questions"
+    ]
+  }, 
+{
+  "topic": "Computer Networks",
+  "days": 5,
+  "tasks": [
+    "Introduction to Computer Networks",
+    "Routing and Switching I",
+    "Routing and Switching II",
+    "Routing and Switching III",
+    "Routing and Switching IV",
+    "Network Topologies",
+    "Network Types",
+    "Network Models",
+    "Network Intrusion Detection and Prevention",
+    "Network Security",
+    "Computer Networks 20 QQ"
+    ]
+  },
+{
+  "topic": "Digital Systems",
+  "days": 20,
+  "tasks": [
+    "Digital Systems Introduction",
+    "Conversion to the Decimal System",
+    "Decimal to Binary Conversion",
+    "Binary Complements",
+    "Binary Subtraction and Addition",
+    "Boolean Algebra Basics",
+    "De Morgan's Theorem for Boolean Algebra",
+    "Truth Tables for Logic Gates and Circuits",
+    "AND and OR Logic Gates",
+    "Exclusive Or (XOR) Logic Gate",
+    "NOT AND (NAND) Logic Gate",
+    "Logic Minimization with Karnaugh Maps (K Maps)",
+    "J-K Flip Flops",
+    "D Flip Flops",
+    "SR Flip Flops",
+    "Mealy and Moore Machines",
+    "State Diagrams",
+    "Counters",
+    "Field Programmable Gate Array (FPGA)",
+    "State Machine Design",
+    "Digital Systems 10 QQ"
+    ]
+  },
+{
+  "topic": "Computer Systems",
+  "days": 2,
+  "tasks": [
+    "Computer Systems Introduction",
+    "Common Abbreviations",
+    "Multicore",
+    "Threading",
+    "Memory & Storage Types",
+    "Caches",
+    "Interfacing",
+    "Computer Systems 10 QQ"
+    ]
+  },
+{
+  "topic": "Software Engineering",
+  "days": 11,
+  "tasks": [
+    "Software Engineering Introduction",
+    "Sorting Algorithms",
+    "Searching Algorithms",
+    "Algorithm Efficiency",
+    "Data Structures Part I",
+    "Data Structures Part II",
+    "Graph Traversals",
+    "Tree Traversals",
+    "Software Syntax Guidelines",
+    "Software Testing",
+    "Software 20 QQ"
+    ]
+  },
+  
+]
