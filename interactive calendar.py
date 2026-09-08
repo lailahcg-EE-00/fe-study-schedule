6f1b08f0edd88976883414e0bcf2c0d34b4c0abc
lailahcg-EE-00 <lailahcg@gmail.com>
2026-09-08 15:48:38 -0700

    Update interactive calendar.py

--- interactive calendar.py
+++ interactive calendar.py
@@ -641,7 +641,7 @@ for week in month_calendar:
 
     for day_index, display_date in enumerate(week):
 
-        with colsif display_date.month != display_month:
+       with cols[day_index]: display_date.month != display_month:
                 st.empty()
                 continue
 
